26a69e0f9342203841ef5a51384cbed9ec82653a
Haribabu05 <sharibabu507@gmail.com>
2024-10-18 23:50:30 +0530

    commit

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Online Javascript Editor for free
-let computer = ['rock','paper','scissor']
-const a = computer.length
-let q = Math.floor(Math.random()*a)
-let player = 'paper'
-console.log(`${player} - ${computer[q]}`)
- const answer = player == computer[q] ? console.log("Tie!") : computer[q] == 'scissor' ? console.log("player Wins") : computer[q] == 'rock' ? console.log("computer Wins") : console.log("Not Tie")
+// // Online Javascript Editor for free
+// let computer = ['rock','paper','scissors']
+// const a = computer.length
+// let q = Math.floor(Math.random()*a)
+// let player = prompt("enter your prompt")
+// if(player)
+// {
+//     console.log(player ?? "enter input");
+    
+// }
 
+// if(player === "paper")
+// {
+// console.log(`${player} - ${computer[q]}`)
+//  const answer = player == computer[q] ? console.log("Tie!") : computer[q] == 'rock' ? console.log("player Wins") : computer[q] == 'scissors' ? console.log("computer Wins") : console.log("Not Tie")
+// }
+// else if(player === "scissors")
+// {
+    
+// console.log(`${player} - ${computer[q]}`)
+// const answer = player == computer[q] ? console.log("Tie!") : computer[q] == 'paper' ? console.log("player Wins") : computer[q] == 'rock' ? console.log("computer Wins") : console.log("Not Tie")
+// }
+// else{
+//       //rock
+// console.log(`${player} - ${computer[q]}`)
+// const answer = player == computer[q] ? console.log("Tie!") : computer[q] == 'scissors' ? console.log("player Wins") : computer[q] == 'paper' ? console.log("computer Wins") : console.log("Not Tie")
+// }
